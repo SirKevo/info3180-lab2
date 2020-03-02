cb83528e7403d4a01a2769d04c9fe88aaa0bf113
Kevar Halstead <kevarhalstead@yahoo.com>
2020-03-01 23:00:18 -0500

    Lab 2 files

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -24,7 +24,16 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html')
 
+@app.route('/datetime/')
+def format_date_joined(date):
+    now = datetime.datetime()
+    date_joined = datetime.date(2020, 2, 29)
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
